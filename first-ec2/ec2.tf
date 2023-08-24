4da81be7d743b163431899923d503e67f2a5530b
Rahul <rahulskumbhar1997@gmail.com>
2023-08-24 21:44:56 +0530

    Added sample file for creating simple EC2 intance

---
+++ first-ec2/ec2.tf
@@ -0,0 +1,12 @@
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource aws_instance "sample-ec2" {
+    instance_type = "t3.micro"
+    ami = "ami-08a52ddb321b32a8c"
+    tags = {
+        Name = "demo-terraform-instance"
+    }
+}
